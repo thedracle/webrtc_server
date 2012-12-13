4f190142f66db835c1066db9ca86d80ed8734d1f
jthomas <jthomas@jthomas-VirtualBox.(none)>
2012-12-13 16:00:53 -0700

    Removed express.

--- webrtc_server.js
+++ webrtc_server.js
@@ -1,4 +1,1 @@
-var app = require('express').createServer();
-app.listen(parseInt(process.env.PORT || '8000'));
-
-webrtc = require('webrtc.io').listen(app);
+webrtc = require('webrtc.io').listen(parseInt(process.env.PORT || '8000'));
