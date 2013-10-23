c532dec1bc4da9689a7d0490db9a34cdb677a505
Ubuntu <ubuntu@ip-10-171-73-244.us-west-1.compute.internal>
2013-10-23 17:00:20 +0000

    Add client command capability.

--- webrtc_server.js
+++ webrtc_server.js
@@ -1,1 +1,54 @@
 webrtc = require('webrtc.io').listen(parseInt(process.env.PORT || '8000'));
+
+webrtc.rtc.on('chat_msg', function(data, socket) {
+  var roomList = webrtc.rtc.rooms[data.room] || [];
+
+  for (var i = 0; i < roomList.length; i++) {
+    var socketId = roomList[i];
+
+    if (socketId !== socket.id) {
+      var soc = webrtc.rtc.getSocket(socketId);
+
+      if (soc) {
+        soc.send(JSON.stringify({
+          "eventName": "receive_chat_msg",
+          "data": {
+            "messages": data.messages,
+            "color": data.color
+          }
+        }), function(error) {
+          if (error) {
+            console.log(error);
+          }
+        });
+      }
+    }
+  }
+});
+
+webrtc.rtc.on('client_command', function(data, socket) {
+  var roomList = webrtc.rtc.rooms[data.room] || [];
+
+  for (var i = 0; i < roomList.length; i++) {
+    var socketId = roomList[i];
+
+    if (socketId !== socket.id) {
+      var soc = webrtc.rtc.getSocket(socketId);
+
+      if (soc) {
+        soc.send(JSON.stringify({
+          "eventName": "receive_client_command",
+          "data": {
+            "command": data.command,
+            "value": data.value,
+            "value2": data.value2
+          }
+        }), function(error) {
+          if (error) {
+            console.log(error);
+          }
+        });
+      }
+    }
+  }
+});
